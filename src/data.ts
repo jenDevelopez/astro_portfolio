192ea2d5686ef3cf8b3912d82d41ae90fc2a99c3
Jennifer <jen1992lo@gmail.com>
2025-09-08 13:28:18 +0200

    Primera version del portfolio

---
+++ src/data.ts
@@ -0,0 +1,43 @@
+export const skills: string[] = [
+  "HTML5",
+  "CSS3",
+  "JavaScript (ES6+)",
+  "TypeScript",
+  "React",
+  "Astro",
+  "PHP",
+  "MySQL",
+  "Python",
+  "Django",
+  "Docker",
+  "WordPress",
+  "WooCommerce",
+  "Git/GitHub",
+];
+export const projects = [
+  {
+    title: "Star Wars Starships",
+    description:
+      "Desarrollo de app de naves de Star Wars, con su informacion, pilotos y películas en la que aparecen",
+    link: "https://star-wars-aplication.netlify.app",
+    githubLink: "https://github.com/jenDevelopez/star-wars/",
+    image: "../public/projects-screenshots/star-wars.webp",
+    skills:['React','TailwindCSS','Firebase','Zustand']
+  },
+    {
+      title: "Javascript quizz",
+      description: "Desarrollo de aplicación de preguntas de javascript para princpiantes",
+      image: "../public/projects-screenshots/js-quizz.webp",
+      link: 'quizz-javascript-app.netlify.app',
+      githubLink: "https://github.com/jenDevelopez/javascript-quizz.git",
+      skills:['React','Zustand','TailwindCSS']
+    },
+    {
+      title: "Poseidrone",
+      description:
+        "Página de Poseidrone, empresa dedicada a la grabacion con drones",
+      link: "https://poseidrone.com/",
+            image: "../public/projects-screenshots/poseidrone.webp",
+      skills:['HTML5','CSS3','JavaScript','JQuery','PHP','Wordpress']
+    },
+];
---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // modo oscuro mediante clase
+  content: [
+    './src/**/*.{astro,html,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
