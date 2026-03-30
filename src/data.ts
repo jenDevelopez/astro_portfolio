6a16b2f502f24688d08fce817604786f1ce62089
Jennifer <jen1992lo@gmail.com>
2026-03-30 15:10:51 +0200

    actualizado email

--- src/data.ts
+++ src/data.ts
@@ -22,30 +22,32 @@ export const projects = [
     link: "https://star-wars-aplication.netlify.app",
     githubLink: "https://github.com/jenDevelopez/star-wars/",
     image: "/projects-screenshots/star-wars.webp",
-    skills:['React','TailwindCSS','Firebase','Zustand']
+    skills: ["React", "TailwindCSS", "Firebase", "Zustand"],
   },
-    {
-      title: "Javascript quizz",
-      description: "Desarrollo de aplicación de preguntas de javascript para princpiantes",
-      image: "/projects-screenshots/js-quizz.webp",
-      link: 'quizz-javascript-app.netlify.app',
-      githubLink: "https://github.com/jenDevelopez/javascript-quizz.git",
-      skills:['React','Zustand','TailwindCSS']
-    },
-    {
-      title: "Poseidrone",
-      description:
-        "Página de Poseidrone, empresa dedicada a la grabacion con drones",
-      link: "https://poseidrone.com/",
-            image: "/projects-screenshots/poseidrone.webp",
-      skills:['HTML5','CSS3','JavaScript','JQuery','PHP','Wordpress']
-    },
-    {
-      title: "Notes App",
-      description:
-        "Página de Poseidrone, empresa dedicada a la grabacion con drones",
-      link: "https://jennotesapp.netlify.app/",
-            image: "/projects-screenshots/notes-app.webp",
-      skills:['Vue','Pinia','CSS3','JavaScript']
-    },
+  {
+    title: "Javascript quizz",
+    description:
+      "Desarrollo de aplicación de preguntas de javascript para princpiantes",
+    image: "/projects-screenshots/js-quizz.webp",
+    link: "quizz-javascript-app.netlify.app",
+    githubLink: "https://github.com/jenDevelopez/javascript-quizz.git",
+    skills: ["React", "Zustand", "TailwindCSS"],
+  },
+  {
+    title: "Poseidrone",
+    description:
+      "Página de Poseidrone, empresa dedicada a la grabacion con drones",
+    link: "https://poseidrone.com/",
+    image: "/projects-screenshots/poseidrone.webp",
+    skills: ["HTML5", "CSS3", "JavaScript", "JQuery", "PHP", "Wordpress"],
+  },
+  {
+    title: "Notes App",
+    description:
+      "Web app de notas aplicando CRUD",
+    link: "https://jennotesapp.netlify.app/",
+    image: "/projects-screenshots/notes-app.webp",
+    githubLink: "https://github.com/jenDevelopez/vue-notes-app.git",
+    skills: ["Vue", "Pinia", "CSS3", "JavaScript"],
+  },
 ];
